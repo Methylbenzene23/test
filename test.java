9c28fc91c2fe93713c5775b9485d087b412fa087
Methylbenzene23 <2320499203@qq.com>
2025-04-17 22:23:01 +0800

    aaaa

--- test.java
+++ test.java
@@ -1,5 +1,12 @@
 public class test {
+    test(){
+        hello();
+    }
     public static void main(String[] args) {
         System.out.println("Hello World");
+    }
+
+    private void hello(){
+        System.out.println("hello");
     }
 }
